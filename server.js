e15b29f29a9f4bf5775884df343dcdf2df3b4dec
Joel Joseph <145049106+N-4284@users.noreply.github.com>
2025-07-03 10:33:22 +0530

    get and post method of attendence (#2)
    Co-authored-by: safdarsidhik <safdarsidhik@gmail.com>

--- server.js
+++ server.js
@@ -28,6 +28,37 @@ app.get('/test-db', async (req, res) => {
     }
 });
 
+
+app.get('/Attendance', async (req,res) =>{
+    try{
+        const result = await GetMethod('SELECT a.attendanceID, a.userID, a.date, lt.leaveTypeName AS status FROM Attendance AS a JOIN LeaveTypes lt ON a.attendanceStatusID = lt.leaveTypeID ORDER BY a.date DESC');
+        res.json(result.recordset);
+    }catch (err){
+        console.error('Error fetching Attendance:', err);
+        res.status(500).send('Failed to get Attendance');
+    }
+    
+});
+
+app.post('/Attendance', async (req, res) => {
+    const { userId, date, attendanceStatusId } = req.body;
+
+    try {
+        const pool = await sql.connect(dbConfig);
+        await pool.request()
+            .input('userID', sql.Int, userId)
+            .input('date', sql.Date, date)
+            .input('attendanceStatusID', sql.Int, attendanceStatusId)
+            .execute('AddAttendance');
+
+        res.status(201).json({ message: 'Attendance marked successfully.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to mark attendance.' });
+    }
+});
+
+
 app.post('/Login', async(req, res) => {
     try{
         const { username, password } = req.body;
